dae9fa784cb2607eab156a91987c8a58a26aa5a6
mamtasuri <mamtasuri@gmail.com>
2023-08-20 19:16:27 -0700

    Add files via upload
    Updated to call modal

--- clinicaltrials_frontend.py
+++ clinicaltrials_frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import modal
+import modal
 import json
 import os
 
@@ -8,7 +8,7 @@ import os
 def main():
     # Set page configuration
     st.set_page_config(
-        page_title="Clinical Trials Viewer",
+        page_title="Clinical Trials Summarizer",
         page_icon="💊",
         layout="wide",
         initial_sidebar_state="expanded",
@@ -29,44 +29,54 @@ def main():
     search_button = st.sidebar.button("Search")
 
     # Display search results
-    st.title("Clinical Trials Viewer")
+    st.title("Clinical Trials Summarizer")
     st.subheader("Deciphering clinical trials can be challenging, but with this site, you can easily view them.")
     st.subheader("             ")
     st.subheader("             ")
-
-    # Read the JSON file for the heart disease data already downloaded and parsed
-    with open('./heartdisease_data.json') as f:
-        heartdisease_data = json.load(f)    
-
-    study_index = st.session_state.get("study_index", 0)
-    if st.button("Next Study"):
-        study_index = (study_index + 1) % len(heartdisease_data)
-        st.session_state.study_index = study_index
-
-    # Display the current study
-    study = heartdisease_data[study_index]
-    st.subheader(f":blue[Study: {study['brief_title']}]")
-    col1, col2 = st.columns([1, 2])  # Create two columns
-    # Left column
-    with col1:
-        st.write(f"**NCT ID:** {study['nct_id']}", unsafe_allow_html=True)
-        st.write(f"**Title:** {study['brief_title']}", unsafe_allow_html=True)
-        st.write(f"**Status:** {study['overall_status']}", unsafe_allow_html=True)
-        st.write(f"**Start Date:** {study['start_date']}", unsafe_allow_html=True)
-        st.write(f"**Completion Date:** {study['completion_date']}", unsafe_allow_html=True)
-        st.write(f"**Lead Sponsor:** {study['lead_sponsor']}", unsafe_allow_html=True)
-
-    # Right column
-    with col2:
-        st.write("**Summary of the Study**")
-        st.write(f" {study['summary']}")
-       # if study['Outcome'] == "Positive" and "improves" in study['Hypothesis']:
-       #     st.write("🟢 Hypothesis Supported", unsafe_allow_html=True)
-       # elif study['Outcome'] == "Negative" and "improves" in study['Hypothesis']:
-       #     st.write("🔴 Hypothesis Not Supported", unsafe_allow_html=True)
-       # else:
-       #     st.write("❓ Outcome and Hypothesis Mismatch", unsafe_allow_html=True) 
     
+    disease_json = None
+    with open('./heartdisease_data.json') as f:
+        disease_json = json.load(f)    
+
+    if search_button:
+        # Call the function to get the study info for the selected disease
+        output_json = process_getstudy_info(selected_disease, additional_term)
+        disease_json = json.load(output_json)
+
+        study_index = st.session_state.get("study_index", 0)
+        if st.button("Next Study"):
+            study_index = (study_index + 1) % len(disease_json)
+            st.session_state.study_index = study_index
+
+        # Display the current study
+        study = disease_json[study_index]
+        st.subheader(f":blue[Study: {study['brief_title']}]")
+        col1, col2 = st.columns([1, 2])  # Create two columns
+        # Left column
+        with col1:
+            st.write(f"**NCT ID:** {study['nct_id']}", unsafe_allow_html=True)
+            st.write(f"**Title:** {study['brief_title']}", unsafe_allow_html=True)
+            st.write(f"**Status:** {study['overall_status']}", unsafe_allow_html=True)
+            st.write(f"**Start Date:** {study['start_date']}", unsafe_allow_html=True)
+            st.write(f"**Completion Date:** {study['completion_date']}", unsafe_allow_html=True)
+            st.write(f"**Lead Sponsor:** {study['lead_sponsor']}", unsafe_allow_html=True)
+
+        # Right column
+        with col2:
+            st.write("**Summary of the Study**")
+            st.write(f" {study['summary']}")
+        # if study['Outcome'] == "Positive" and "improves" in study['Hypothesis']:
+        #     st.write("🟢 Hypothesis Supported", unsafe_allow_html=True)
+        # elif study['Outcome'] == "Negative" and "improves" in study['Hypothesis']:
+        #     st.write("🔴 Hypothesis Not Supported", unsafe_allow_html=True)
+        # else:
+        #     st.write("❓ Outcome and Hypothesis Mismatch", unsafe_allow_html=True) 
+ 
+ 
+def process_getstudy_info(selected_disease, additional_term):
+    f = modal.Function.lookup("clinicaltrials-project", "get_study_info")
+    output = f.call(selected_disease, additional_term)
+    return output   
 
 if __name__ == "__main__":
     main()
